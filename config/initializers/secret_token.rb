e9603632fde2c6ae679f2f0c7e1871c9f314d9d7
100lp <spb.gurme@gmail.com>
2013-08-24 14:42:41 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Italico::Application.config.secret_token = '397b035db236488dc5c67d14b2083eafba5a57634e315ffe8fb278c20e06a899fb3ddd4a4f6ee591f48aa9b851bd7241a3f05721ddce191fe253f6d45158b137'
